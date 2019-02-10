434e2c91aa0a1b90b6a815e0a4be72ced3e5360f
takeshi0406 <sci.and.eng@gmail.com>
2019-02-10 18:08:19 +0900

    first music

--- beep.c
+++ beep.c
@@ -2,6 +2,12 @@
 #include <gb/drawing.h>
 
 int main(void) {
+    char DOREMI[]={
+        131, 131, 273, 273, 455, 455, 273, 0,
+        201, 201, 180, 180, 150, 150, 131, 0
+    };
+    int i = 0;
+
     NR50_REG = 0xFF;
     NR51_REG = 0xFF;
     NR52_REG = 0x80;
@@ -11,31 +17,19 @@ int main(void) {
     
     gotogxy(2, 3);
     gprintf("Press any button");
+
+
     
     while (1) {
         if (joypad()) {        
-            NR10_REG = 0x38U;
-            // NR10_REG = 0x39U;
+
             NR11_REG = 0x70U;
             NR12_REG = 0xE0U;
-            NR13_REG = 0x0AU;
-            NR14_REG = 0xC7U;
-
-            // NR51_REG |= 0x0AU;
-            
-            delay(200);
-            NR13_REG = 0;
-            NR14_REG = 0xC7U;
-            delay(200);
-
-            NR13_REG = 1;
-            NR14_REG = 0xC7U;
-            delay(200);
-
-            NR13_REG = 2;
-            NR14_REG = 0xC7U;
-            delay(200);
-            
+            NR13_REG = 0x0BU;
+            if (DOREMI[i]) NR14_REG = DOREMI[i] | 0x80;
+            NR51_REG |= 0x0AU;
+            delay(400);
+            i++;
         }
     }
 }
