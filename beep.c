b303f4f355c5d5832208bd005af99fb8d257b3bc
takeshi0406 <sci.and.eng@gmail.com>
2019-02-17 20:17:25 +0900

    noise

--- beep.c
+++ beep.c
@@ -1,35 +1,21 @@
 #include <gb/gb.h>
-#include <gb/drawing.h>
+#include <stdio.h>
 
-int main(void) {
-    char DOREMI[]={
-        131, 131, 273, 273, 455, 455, 273, 0,
-        201, 201, 180, 180, 150, 150, 131, 0
-    };
-    int i = 0;
-
-    NR50_REG = 0xFF;
-    NR51_REG = 0xFF;
+void main(){
     NR52_REG = 0x80;
-    
-    gotogxy(1, 1);
-    gprintf("====== Beep ======");
-    
-    gotogxy(2, 3);
-    gprintf("Press any button");
+    NR50_REG = 0x77;
+    NR51_REG = 0xFF;
 
+    while(1) {
+        UBYTE joypad_state = joypad();
+        
+        if(joypad_state) {
+            NR41_REG = 0x1F;
+            NR42_REG = 0xF1;
+            NR43_REG = 0x30;
+            NR44_REG = 0xC0;  
 
-    
-    while (1) {
-        if (joypad()) {        
-
-            NR11_REG = 0x70U;
-            NR12_REG = 0xE0U;
-            NR13_REG = 0x0BU;
-            if (DOREMI[i]) NR14_REG = DOREMI[i] | 0x80;
-            NR51_REG |= 0x0AU;
-            delay(400);
-            i++;
+            delay(1);
         }
     }
 }
