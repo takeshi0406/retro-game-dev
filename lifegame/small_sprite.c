13955e9651b12ab1af6b6f7640b27b2ce2c210bc
takeshi0406 <sci.and.eng@gmail.com>
2019-03-16 15:06:41 +0900

    tmp

--- lifegame/small_sprite.c
+++ lifegame/small_sprite.c
@@ -7,4 +7,9 @@ void main() {
     set_sprite_tile(0, 0);
     move_sprite(0, 50, 50);
     SHOW_SPRITES;
+    delay(400);
+    set_sprite_data(0, 8, sprite);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 100, 100);
+    SHOW_SPRITES;
 }
