3ab3145758e5f21e1bf6634b74b2ec70487730dc
takeshi0406 <sci.and.eng@gmail.com>
2019-03-18 23:15:11 +0900

    multiple

--- lifegame/small_sprite.c
+++ lifegame/small_sprite.c
@@ -1,17 +1,15 @@
 #include <gb/gb.h>
 #include "sprite.c"
 
-void main() {
-    SPRITES_8x8;
-    set_sprite_data(0, 8, sprite);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 50, 50);
-    SHOW_SPRITES;
-    
-    SPRITES_8x8;
-    delay(400);
-    set_sprite_data(0, 8, sprite);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 100, 100);
+
+void main(){
+	SPRITES_8x8;
+	set_sprite_data(0, 8, sprite);
+	set_sprite_tile(0, 0);
+	move_sprite(1, 55 + 8, 75);
+		
+	set_sprite_data(8, 8, sprite);
+	set_sprite_tile(2, 8);
+	move_sprite(2, 95, 75);
     SHOW_SPRITES;
 }
