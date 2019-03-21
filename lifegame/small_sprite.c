583d836c580d2d0b2ed7541821bfc4168bd92d01
takeshi0406 <sci.and.eng@gmail.com>
2019-03-21 20:00:11 +0900

    tile

--- lifegame/small_sprite.c
+++ lifegame/small_sprite.c
@@ -3,13 +3,15 @@
 
 
 void main(){
-    UBYTE i;
+    UBYTE i, j;
     SPRITES_8x8;
     set_sprite_data(0, 0, sprite);
     set_sprite_tile(0, 0);
-    for (i = 0; i != 20; i++) {
-        move_sprite(i, i * 8, i * 8 + 8);
-        SHOW_SPRITES;
-        delay(100);
+    for (i = 0; i != 8; i++) {
+        for (j = 0; j != 8; j++) {
+            move_sprite(i * 8 + j, i * 8 + 8, j * 8 + 8);
+            SHOW_SPRITES;
+            delay(100);
+        }
     }
 }
