c0af08c23dd2a7b37332ff72cda95d21d9bd991a
takeshi0406 <sci.and.eng@gmail.com>
2019-03-23 17:16:09 +0900

    tmp

--- lifegame/small_sprite.c
+++ lifegame/small_sprite.c
@@ -1,13 +1,19 @@
 #include <gb/gb.h>
-// #include "sprite.c"
 #include <gb/drawing.h>
+#include "sprite.c"
 
 
 void main(){
     UBYTE i, j;
-    for (i = 0; i != 4; i++) {
-        for (j = 0; j != 8; j++) {
-            gprintf("Hello World!");
+    SPRITES_8x16;
+    // set_sprite_data(0, 0, sprite);
+    // set_sprite_tile(0, 0);
+    for (i = 0; i != 100; i++) {
+        for (j = 0; j != 100; j++) {
+            box(10, 10, 50, 50, M_NOFILL);
+            // plot_point(20 + i, 20 + j);
+            // move_sprite(i * 8 + j, i * 8 + 20, j * 16 + 20);
+            // SHOW_SPRITES;
         }
     }
 }
