c4c4b5008013eb23d3ec2a58e455b1c10eab1c4b
takeshi0406 <sci.and.eng@gmail.com>
2019-03-24 22:52:56 +0900

    tmp

--- lifegame/lifegame.c
+++ lifegame/lifegame.c
@@ -2,17 +2,37 @@
 #include <gb/gb.h>
 #include <gb/drawing.h>
 
+#define X_MIN 4
+#define X_MAX 156
+#define Y_MIN 4
+#define Y_MAX 140
+
+UBYTE x1;
+UBYTE y1;
+
+
+void init();
+
 void main() {
-    UBYTE x1 = 4;
-    UWORD y1 = 4;
+    init();
+}
 
+void init() {
     color(BLACK, WHITE, SOLID);
+    for (x1 = X_MIN; x1 <= X_MAX; x1 += 8) {
+        line(x1, Y_MIN, x1, Y_MAX);
+        delay(16);
+    }
 
-    for (x1 = 4; x1 < 160; x1 += 8) {
-        line(x1, 4, x1, 152);
-        for (y1 = 4; y1 < 160; y1 += 8) {
-            line(4, y1, 152, y1);
+    for (y1 = Y_MIN; y1 <= Y_MAX; y1 += 8) {
+        line(X_MIN, y1, X_MAX, y1);
+        delay(16);
+    }
+
+    for (x1 = X_MIN; x1 <= X_MAX; x1 += 8) {
+        for (y1 = Y_MIN; y1 <= Y_MAX; y1 += 8) {
             circle(x1, y1, 2, M_FILL);
+            delay(1);
         }
     }
 }
