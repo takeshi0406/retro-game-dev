096958317ece400c256823163c25d1627688f618
takeshi0406 <sci.and.eng@gmail.com>
2019-03-25 22:53:01 +0900

    tmp

--- lifegame/lifegame.c
+++ lifegame/lifegame.c
@@ -2,13 +2,17 @@
 #include <gb/gb.h>
 #include <gb/drawing.h>
 
+#define RADIUS 2
 #define X_MIN 4
 #define X_MAX 156
+#define X_NODES (X_MAX - X_MIN) / 8 + 1
 #define Y_MIN 4
 #define Y_MAX 140
+#define Y_NODES (Y_MAX - Y_MIN) / 8 + 1
 
 UBYTE x1;
 UBYTE y1;
+UBYTE map[X_NODES][Y_NODES] = {0};
 
 
 void init();
@@ -18,7 +22,6 @@ void main() {
 }
 
 void init() {
-    color(BLACK, WHITE, SOLID);
     for (x1 = X_MIN; x1 <= X_MAX; x1 += 8) {
         line(x1, Y_MIN, x1, Y_MAX);
         delay(16);
@@ -31,7 +34,8 @@ void init() {
 
     for (x1 = X_MIN; x1 <= X_MAX; x1 += 8) {
         for (y1 = Y_MIN; y1 <= Y_MAX; y1 += 8) {
-            circle(x1, y1, 2, M_FILL);
+            color(WHITE, WHITE, SOLID);
+            circle(x1, y1, RADIUS, M_FILL);
             delay(1);
         }
     }
