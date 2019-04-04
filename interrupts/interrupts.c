05cea1ccf9c8c0ee98b296696858f3a97bb75302
takeshi0406 <sci.and.eng@gmail.com>
2019-04-04 09:28:09 +0900

    途中

--- interrupts/interrupts.c
+++ interrupts/interrupts.c
@@ -7,13 +7,13 @@ UWORD count;
 void joy_isr(void);
 
 int main(void) {
-    gprintf("1");
-    gprintf("2");
+    gprintf("aaa");
+    delay(10);
     disable_interrupts();
     add_JOY(joy_isr);
     enable_interrupts();
     set_interrupts(JOY_IFLAG);
-    gprintf("3");
+
     // see https://github.com/hschmitt/gbdk/blob/d7707bf2b3c5433edcd37d53a1475fd51a9b9502/examples-gb/irq.c
     /*
     return 0;
