e55b206949e6aadeb633dc2d0292525a8d0de5b8
takeshi0406 <sci.and.eng@gmail.com>
2019-04-11 08:56:08 +0900

    tmp

--- polling/polling.c
+++ polling/polling.c
@@ -8,23 +8,17 @@ UWORD known;
 void joy_isr(void);
 
 int main(void) {
-    disable_interrupts();
-    add_JOY(joy_isr);
-    enable_interrupts();
-    set_interrupts(JOY_IFLAG);
     count = 0;
     known = 0;
 
-    // see https://github.com/hschmitt/gbdk/blob/d7707bf2b3c5433edcd37d53a1475fd51a9b9502/examples-gb/irq.c
-    while (1) {
+    for (;;) {
+        if (joypad() == J_A) {
+            count++;
+        }
         if (count != known) {
             printf("count %d\n", count);
             known = count;
         }
         delay(100);
     }
-}
-
-void joy_isr(void) {
-    count++;
 }
