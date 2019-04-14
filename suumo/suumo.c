04d546ea9a69c51d419f268b891ed7bb6d214959
takeshi0406 <sci.and.eng@gmail.com>
2019-04-14 20:44:00 +0900

    なぜかうまくいかない

--- suumo/suumo.c
+++ suumo/suumo.c
@@ -4,13 +4,28 @@
 #include "stdio.h"
 
 
+Sound sound;
+void update_music(void) {
+    //printf("test");
+    beep(1000);
+    //Sound_play(&sound);
+}
+
 int main(void) {
     Player player;
-    Sound sound;
     Player_init(&player, 0, 50, 75);
     Sound_init(&sound);
+
+    // 初期化
+    disable_interrupts();
+    add_TIM(update_music);
+    set_interrupts(TIM_IFLAG);
+    enable_interrupts();
+
     for (;;) {
         Player_move(&player, joypad());
-        Sound_play(&sound);
     }
 }
+
+
+
