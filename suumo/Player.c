ea1fe7697ddd19730ff7b46352649c81b2f9ccb3
takeshi0406 <sci.and.eng@gmail.com>
2019-04-17 22:20:56 +0900

    tmp

--- suumo/Player.c
+++ suumo/Player.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 
 unsigned char sprite[] = {
-    0x5A,0x3C,0xE3,0x42,0x7C,0x99,0xEB,0xA5,
-    0xFB,0xA5,0x66,0x99,0xE7,0x42,0x5A,0x3C
+    0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x0F,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x0F,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0xFB,0x0F,0x66,0xFF,0xE7,0x42,0xFF,0xFF
 };
 
 void Player_init(Player* player, UBYTE i, UBYTE x, UBYTE y) {
@@ -12,10 +14,13 @@ void Player_init(Player* player, UBYTE i, UBYTE x, UBYTE y) {
     player->i = i * 1;
     player->x = x * 1;
     player->y = y * 1;
-    SPRITES_8x8;
-    set_sprite_data(0, 0, sprite);
-    set_sprite_tile(0, 0);
+    SPRITES_8x16;
+    set_sprite_data(player->i, 0, sprite);
+    set_sprite_tile(player->i, 0);
+    set_sprite_data(player->i + 1, 1, sprite);
+    set_sprite_tile(player->i + 1, 1);
     move_sprite(player->i, player->x, player->y);
+    move_sprite(player->i + 1, player->x + 7, player->y);
     SHOW_SPRITES;
 }
 
@@ -31,6 +36,7 @@ void Player_move(Player* player, UBYTE joystate) {
     else if (joystate & J_DOWN) player->y++;
 
     move_sprite(player->i, player->x, player->y);
+    move_sprite(player->i + 1, player->x + 7, player->y);
     delay(10);
 }
 
