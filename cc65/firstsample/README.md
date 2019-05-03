a54e1fcc374cd965c953830961ae5b9a4f32ff8e
takeshi0406 <sci.and.eng@gmail.com>
2019-05-03 12:50:57 +0900

    nesfirst

---
+++ cc65/firstsample/README.md
@@ -0,0 +1,3 @@
+# firstsample
+
+https://www34.atwiki.jp/cc65/pages/14.html
---
+++ cc65/firstsample/main.c
@@ -0,0 +1,10 @@
+#include <conio.h>
+int main (void)
+{
+    clrscr();
+    cprintf("hello cc65");
+    while(1)
+    {
+    }
+    return 0;
+}
