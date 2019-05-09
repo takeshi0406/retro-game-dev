054618f6283ae1de37d9a1ab7c7f291297d7c1b3
takeshi0406 <sci.and.eng@gmail.com>
2019-05-09 20:28:54 +0900

    tmp

--- gbdk/automusic/main.c
+++ gbdk/automusic/main.c
@@ -8,7 +8,11 @@ int main(void) {
     Piece_init(&piece[0], 0, 50, 50);
     Piece_init(&piece[1], 1, 55, 55);
     for (;;) {
-        Piece_move(&piece, 1, 1);
+        Piece_move(&piece[0], 1, 0);
+        Piece_move(&piece[1], 1, 0);
+        delay(100);
+        Piece_move(&piece[0], -1, 0);
+        Piece_move(&piece[1], -1, 0);
         delay(100);
     }
 }
--- gbdk/automusic/piece.c
+++ gbdk/automusic/piece.c
@@ -1,18 +1,19 @@
 #include "piece.h"
+#include "stdio.h"
 
 const unsigned char sprite[] = {
-    0x00, 0x00, 0x00, 0x00,
+    0xFF, 0xFF, 0xFF, 0xFF,
     0x00, 0x00, 0x00, 0x00,
     0x00, 0x1F, 0x00, 0x3F,
     0x00, 0x7F, 0x00, 0x77
 };
 
 void Piece_init(Piece* piece, UBYTE i, UBYTE x, UBYTE y) {
-    piece->i = i;
-    piece->x = x;
-    piece->y = y;
+    piece->i = i * 1;
+    piece->x = x * 1;
+    piece->y = y * 1;
     SPRITES_8x8;
-    set_sprite_data(i, 0, sprite);
+    set_sprite_data(piece->i, 0, sprite);
     set_sprite_tile(piece->i, 0);
     move_sprite(piece->i, piece->x, piece->y);
     SHOW_SPRITES;
@@ -21,4 +22,5 @@ void Piece_init(Piece* piece, UBYTE i, UBYTE x, UBYTE y) {
 void Piece_move(Piece* piece, UBYTE dx, UBYTE dy) {
     piece->x += dx;
     piece->y += dy;
+    move_sprite(piece->i, piece->x, piece->y);
 }
